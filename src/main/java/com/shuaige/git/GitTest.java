566b6df6f4e376c6c13d34e14a6b3433a48c8242
shuaige <shuaige@shuaige.com>
2023-02-16 14:37:02 +0800

    second commit

--- src/main/java/com/shuaige/git/GitTest.java
+++ src/main/java/com/shuaige/git/GitTest.java
@@ -3,5 +3,6 @@ package com.shuaige.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("hello,git!");
     }
 }
