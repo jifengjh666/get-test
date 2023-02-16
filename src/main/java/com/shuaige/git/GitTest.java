e779074fce0aad4a78199eada19fed886d7527de
jifengjh666 <125531750+jifengjh666@users.noreply.github.com>
2023-02-16 16:23:24 +0800

    pull commit

--- src/main/java/com/shuaige/git/GitTest.java
+++ src/main/java/com/shuaige/git/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
         System.out.println("hello,git!");
         System.out.println("hello,git!");
         System.out.println("hello,git!");
-        System.out.println("hello,git!");
+        System.out.println("pull test!");
     }
 }
